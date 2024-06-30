5d198cfc6acd18c8c1721d197e2c473a7c8dcf6f
Rayhan-Ahmed-7 <dev.rayhan7@gmail.com>
2024-06-30 22:16:44 +0600

    :bug: navigation bug fixed

--- src/utils/authUtils.ts
+++ src/utils/authUtils.ts
@@ -3,17 +3,18 @@ import Urls from "../helper/urls";
 import AxiosService from "../services/axios_service";
 
 const authUtils = {
-    isAuthenticated: async () => {
-        try {
-            const token = localStorage.getItem(ACCESS_TOKEN)
-            if (!token) {
-                return false;
-            }
-            const user = await AxiosService.get(Urls.verifyToken);
-            return user;
-        } catch (err) {
-            return false;
-        }
+  isAuthenticated: async () => {
+    try {
+      const token = localStorage.getItem(ACCESS_TOKEN);
+      if (!token) {
+        return false;
+      }
+      const user = await AxiosService.get(Urls.verifyToken);
+
+      return user;
+    } catch (err) {
+      return false;
     }
-}
+  },
+};
 export default authUtils;
--- src/views/feature/board/hooks/useBoard.tsx
+++ src/views/feature/board/hooks/useBoard.tsx
@@ -7,73 +7,80 @@ import { useNavigate, useParams } from "react-router-dom";
 import { IBoard } from "../types";
 
 const useBoard = () => {
-    const boardApi = new BoardAPI();
-    const navigate = useNavigate();
-    const { boardId } = useParams();
-    const [activeIndex, setActiveIndex] = useState(0);
-    const [loading, setLoading] = useState(DataStatus.idle);
-    const boards = useSelector(state => state.board);
-    useEffect(() => {
-        getBoards();
-    }, [])
-    useEffect(() => {
-        const activeItem = boards.findIndex(e => e.id === boardId)
-        setActiveIndex(activeItem)
-    }, [boardId])
-    useEffect(() => {
-        if (boards.length > 0) {
-            navigate(`/boards/${boards?.[0]?.id}`)
-        }
-        if (boards.length == 0) {
-            navigate("/boards")
-        }
-    }, [boards])
-
-    const createBoard = async ({ userId }: { userId: string }) => {
-        try {
-            setLoading(DataStatus.loading)
-            let response = await boardApi.createBoard({ userId });
-            let newList = [response?.data?.data, ...boards];
-            dispatch(setBoards(newList));
-            navigate(`/boards/${response?.data?.data?.id}`)
-            setLoading(DataStatus.loaded)
-        } catch (error) {
-            console.log(error);
-            setLoading(DataStatus.error)
-        }
+  const boardApi = new BoardAPI();
+  const navigate = useNavigate();
+  const { boardId } = useParams();
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [loading, setLoading] = useState(DataStatus.idle);
+  const boards = useSelector((state) => state.board);
+  useEffect(() => {
+    getBoards();
+  }, []);
+  useEffect(() => {
+    const activeItem = boards.findIndex((e) => e.id === boardId);
+    setActiveIndex(activeItem);
+  }, [boardId]);
+  useEffect(() => {
+    if (boards.length > 0) {
+      navigate(`/boards/${boards?.[0]?.id}`);
     }
-
-    const getBoards = async () => {
-        try {
-            setLoading(DataStatus.loading)
-            let response = await boardApi.getBoards();
-            dispatch(setBoards(response.data?.data))
-            setLoading(DataStatus.loaded)
-        } catch (error) {
-            setLoading(DataStatus.error)
-        }
+    if (boards.length == 0) {
+      navigate("/boards");
     }
+  }, [boards.length]);
 
-
-    const updateActive = (boards: IBoard[]) => {
-        const activeIndex = boards.findIndex(e => e.id == boardId);
-        setActiveIndex(activeIndex);
+  const createBoard = async ({ userId }: { userId: string }) => {
+    try {
+      setLoading(DataStatus.loading);
+      let response = await boardApi.createBoard({ userId });
+      let newList = [response?.data?.data, ...boards];
+      dispatch(setBoards(newList));
+      navigate(`/boards/${response?.data?.data?.id}`);
+      setLoading(DataStatus.loaded);
+    } catch (error) {
+      console.log(error);
+      setLoading(DataStatus.error);
     }
+  };
 
-    const onDragEnd = async ({ source, destination }: any) => {
-        const newList = [...boards];
-        const [removed] = newList.splice(source.index, 1);
-        newList.splice(destination.index, 0, removed);
-        const activeIndex = newList.findIndex(e => e.id == boardId);
-        setActiveIndex(activeIndex);
-        dispatch(setBoards(newList));
-        try {
-            await boardApi.updateBoards({ boards: newList })
-        } catch (error) {
-            console.log(error)
-        }
+  const getBoards = async () => {
+    try {
+      setLoading(DataStatus.loading);
+      let response = await boardApi.getBoards();
+      dispatch(setBoards(response.data?.data));
+      setLoading(DataStatus.loaded);
+    } catch (error) {
+      setLoading(DataStatus.error);
     }
-    return { createBoard, getBoards, loading, activeIndex, setActiveIndex, updateActive, onDragEnd }
+  };
+
+  const updateActive = (boards: IBoard[]) => {
+    const activeIndex = boards.findIndex((e) => e.id == boardId);
+    setActiveIndex(activeIndex);
+  };
+
+  const onDragEnd = async ({ source, destination }: any) => {
+    const newList = [...boards];
+    const [removed] = newList.splice(source.index, 1);
+    newList.splice(destination.index, 0, removed);
+    const activeIndex = newList.findIndex((e) => e.id == boardId);
+    setActiveIndex(activeIndex);
+    dispatch(setBoards(newList));
+    try {
+      await boardApi.updateBoards({ boards: newList });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return {
+    createBoard,
+    getBoards,
+    loading,
+    activeIndex,
+    setActiveIndex,
+    updateActive,
+    onDragEnd,
+  };
 };
 
 export default useBoard;
